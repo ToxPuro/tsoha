30cd7d480ee8c6d339a0736321c746613af8c89a
Puro Touko O <toxpuro@cubbli20-gold.local>
2021-09-08 11:13:29 +0300

    setup registering and login

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE TABLE messages (id SERIAL PRIMARY KEY, content TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
--- src/app.py
+++ src/app.py
@@ -1,26 +1,52 @@
 from flask import Flask
-from flask import redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, render_template, request, session
 from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
 db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT content FROM messages")
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages) 
+    return render_template("index.html")
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
-    db.session.commit()
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if not user:
+        return redirect("/")
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["username"] = username
+        else:
+            return redirect("/")
+        
     return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hash_value = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+        return redirect("/")
+
